08670a819b7d8c60a4397722de3d9197483b39b5
Karen Zapata <kzapata0919@gmail.com>
2017-11-08 09:57:49 -0800

    Add Player test for should have a board

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,33 @@
 require "./player"
+require_relative "board_spec"
 
 describe Player do
-  player_1 = Player.new("X")
 
+  board_structure = {1 => [], 2 => [], 3 =>[]}
+  my_board = Board.new(board_structure)
 
+  player_1 = Player.new(my_board, "X")
+
+  it "should have a board" do
+    expect(player_1.board).to eq(board_structure)
+  end
+
+# #   context "if location is empty" do
+
+# #     it "should place mark on target location" do
+# #       expect(player_1.mark_board(1, 0)).to eq("successfully marked board")
+# #     end
+
+# #   end
+
+# #   context "if location is occupied" do
+
+# #     player_1.mark_board(1, 0)
+
+# #     it "should ask for a new location" do
+# #       expect(player_1.mark_board(1, 0)).to eq("space occupied, find a new location")
+# #     end
+
+# #   end
 
 end
