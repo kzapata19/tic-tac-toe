5f95efb15c8d8139351da8c4300743b96fcb30ce
Karen Zapata <kzapata0919@gmail.com>
2017-11-07 19:08:55 -0800

    Refactor Board class and refactor Board test for emptiness

--- board.rb
+++ board.rb
@@ -6,36 +6,28 @@ class Board
     @board = board
   end
 
-# make method below private
-  def get_target_row(row)
-    board[row]
-  end
-
-# make method below private
-  def find_target_location(row, column)
-    get_target_row(row)[column]
-  end
-
-  def mark_board(symbol, row, column)
-
-    location = find_target_location(row, column)
-
-    if location == nil
-      get_target_row(row)[column] = symbol
-      "successfully placed '#{symbol}'"
-    # later refactor else block to execute play until location is available
-    else
-      "space occupied, provide a new location"
-    end
-
-  end
-
 end
 
-# board_structure = {1 => [], 2 => [], 3 =>[]}
-# my_board = Board.new(board_structure)
+# # make method below private
+#   def get_target_row(row)
+#     board[row]
+#   end
 
-# puts my_board
-# puts my_board.find_target_location(1,0)
-# puts my_board.mark_board("X", 1, 0)
-# puts my_board.board
+# # make method below private
+#   def find_target_location(row, column)
+#     get_target_row(row)[column]
+#   end
+
+#   def mark_board(symbol, row, column)
+
+#     location = find_target_location(row, column)
+
+#     if location == nil
+#       get_target_row(row)[column] = symbol
+#       "successfully placed '#{symbol}'"
+#     # later refactor else block to execute play until location is available
+#     else
+#       "space occupied, provide a new location"
+#     end
+
+#   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,38 +4,42 @@ require "./board"
 describe Board do
 
   board_structure = {1 => [], 2 => [], 3 =>[]}
-  my_board = Board.new(board_structure)
+  board_1 = Board.new(board_structure)
+  board = board_1.board
 
   context "at the start of the game" do
 
-    it "should be empty" do
-      expect(my_board.board.length).to eq(3)
-    end
+    it "board should be empty" do
+      length = 0
+      board.each_value {|row| length = row.length + length}
 
-  end
-
-# should I test find_target_location if its a helper method for play? Prob not?
-  it "should find target location on board" do
-    expect(my_board.find_target_location(1, 0)).to eq(nil)
-  end
-
-# definitely test play since method will be publicly available
-  context "if target location is not occupied" do
-
-    it "should insert symbol in target location" do
-      expect(my_board.mark_board("X", 1, 0)).to eq("successfully placed 'X'")
-    end
-
-  end
-
-  context "if target location is occupied" do
-
-    it "should ask for a new target location" do
-      my_board.mark_board("X", 1, 0)
-      expect(my_board.mark_board("O", 1, 0)).to eq("space occupied, provide a new location")
+      expect(length).to eq(0)
     end
 
   end
 
 end
+
+# # should I test find_target_location if its a helper method for play? Prob not?
+#   it "should find target location on board" do
+#     expect(my_board.find_target_location(1, 0)).to eq(nil)
+#   end
+
+# # definitely test play since method will be publicly available
+#   context "if target location is not occupied" do
+
+#     it "should insert symbol in target location" do
+#       expect(my_board.mark_board("X", 1, 0)).to eq("successfully placed 'X'")
+#     end
+
+#   end
+
+#   context "if target location is occupied" do
+
+#     it "should ask for a new target location" do
+#       my_board.mark_board("X", 1, 0)
+#       expect(my_board.mark_board("O", 1, 0)).to eq("space occupied, provide a new location")
+#     end
+
+#   end
 
