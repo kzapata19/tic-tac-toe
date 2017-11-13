dde0b20f4d279c24634df035c0c704baf11197e7
Karen Zapata <kzapata0919@gmail.com>
2017-11-12 19:33:06 -0800

    Add .rspec

--- board.rb
+++ board.rb
@@ -7,27 +7,3 @@ class Board
   end
 
 end
-
-# # make method below private
-#   def get_target_row(row)
-#     board[row]
-#   end
-
-# # make method below private
-#   def find_target_location(row, column)
-#     get_target_row(row)[column]
-#   end
-
-#   def mark_board(symbol, row, column)
-
-#     location = find_target_location(row, column)
-
-#     if location == nil
-#       get_target_row(row)[column] = symbol
-#       "successfully placed '#{symbol}'"
-#     # later refactor else block to execute play until location is available
-#     else
-#       "space occupied, provide a new location"
-#     end
-
-#   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -19,27 +19,3 @@ describe Board do
   end
 
 end
-
-# # should I test find_target_location if its a helper method for play? Prob not?
-#   it "should find target location on board" do
-#     expect(my_board.find_target_location(1, 0)).to eq(nil)
-#   end
-
-# # definitely test play since method will be publicly available
-#   context "if target location is not occupied" do
-
-#     it "should insert symbol in target location" do
-#       expect(my_board.mark_board("X", 1, 0)).to eq("successfully placed 'X'")
-#     end
-
-#   end
-
-#   context "if target location is occupied" do
-
-#     it "should ask for a new target location" do
-#       my_board.mark_board("X", 1, 0)
-#       expect(my_board.mark_board("O", 1, 0)).to eq("space occupied, provide a new location")
-#     end
-
-#   end
-
