a60b3dc3e0f47d355fd8553e0de9f4d5d659628e
Karen Zapata <kzapata0919@gmail.com>
2017-11-14 10:37:24 -0800

    Add game_session and spec

--- lib/game_session.rb
+++ lib/game_session.rb
@@ -1,1 +1,25 @@
 # this class will make players and board interact
+class GameSession
+
+  attr_accessor :player1, :player2, :board, :rules, :display
+
+  def initialize(player1, player2, board, rules, display)
+    @player1 = Player.new("X")
+    @player2 = Player.new("O")
+    @board = Board.new(3, 3)
+    @rules = Rules.new(@board)
+    @display = Display.new(@board)
+  end
+
+  def start_game
+    p "Welcome to TicTacToe! Player 1 is 'X' and Player 2 is 'O'."
+  end
+
+  #ask for player's coordinates
+  #begin
+
+
+
+  #run Rules.check_win
+
+end
---
+++ spec/game_session_spec.rb
@@ -0,0 +1,20 @@
+describe GameSession do
+
+  before(:each) do
+    @player1 = Player.new("X")
+    @player2 = Player.new("O")
+    @board = Board.new(3, 3)
+    @rules = Rules.new(@board)
+    @display = Display.new(@board)
+    @game_session = GameSession.new(@player1, @player2, @board, @rules, @display)
+  end
+
+  it "should output welcome message at start of game" do
+    expect(@game_session.start_game).to eq("Welcome to TicTacToe! Player 1 is 'X' and Player 2 is 'O'.")
+  end
+
+  # it "should ask for Player 1's first move" do
+
+  # end
+
+end
