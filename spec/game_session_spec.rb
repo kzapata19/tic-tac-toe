6b2514b54e8dd4a95e723a48544190615f58a36e
Karen Zapata <kzapata0919@gmail.com>
2017-11-16 16:42:21 -0800

    Remove test for case when current_board status is neither win nor tie (since still looping through game)

--- spec/game_session_spec.rb
+++ spec/game_session_spec.rb
@@ -12,18 +12,15 @@ describe GameSession do
     @game_session = GameSession.new(@player1, @player2, @board, @rules, @display)
   end
 
-  # it "should output welcome message at start of game" do
-  #   expect(@game_session.start_game).to eq("Welcome to TicTacToe! Player 1 is 'X' and Player 2 is 'O'.")
+#might not need this test since redefining get_player_move to only print prompt message and save player's move only
+  # it "should ask Player 1 for their first move" do
+  #   expect(@game_session.get_player_move(@player1)).to eq("\n
+  #    _X_|_2_|_3_\n
+
+  #    _4_|_5_|_6_\n
+
+  #    _7_|_8_|_9_\n\n")
   # end
-
-  it "should ask Player 1 for their first move" do
-    expect(@game_session.get_player_move(@player1)).to eq("\n
-     _X_|_2_|_3_\n
-
-     _4_|_5_|_6_\n
-
-     _7_|_8_|_9_\n\n")
-  end
 
   it "should mark the board's grid" do
     expect(@game_session.update_board_grid(1, @player1)).to eq([['X', nil, nil], [nil, nil, nil], [nil, nil, nil]])
@@ -32,12 +29,6 @@ describe GameSession do
   # it "should mark the board's grid" do
   #   expect(@game_session.update_board_grid(5, @player1)).to eq([[nil, nil, nil], [nil, 'X', nil], [nil, nil, nil]])
   # end
-
-  it "should display current game status" do
-    @game_session.get_player_move(@player1) #this will be necessary once location = 1 is removed from game_session
-
-    expect(@game_session.current_board_status).to eq("No winner or tie yet. Next..")
-  end
 
   it "should display 'X' as winner of game" do
     @game_session.board.grid = [['X', 'O', 'X'], ['X', 'O', 'O'], ['X', 'X', 'O']]
@@ -62,10 +53,11 @@ describe GameSession do
     expect(@game_session.current_player).to eq(@game_session.player2)
   end
 
-  it "should ask for Player 2's move if no winner or tie" do
-    @game_session.run_game_loop
-    expect(@game_session.current_player).to eq(@game_session.player2)
-  end
+  # it "should ask for Player 2's move if no winner or tie" do
+  #   @game_session.board = [['X', 'X', 'O'], ['O', 'O', 'X'], ['X', 'O', 'X']]
+  #   @game_session.run_game_loop
+  #   expect(@game_session.current_player).to eq(@game_session.player2)
+  # end
 
 
 end
