8d99e97c1d7bf03c1b770bef8b93cc08eea8209f
Karen Zapata <kzapata0919@gmail.com>
2017-11-22 11:46:49 -0800

    Refactor provide_move method to handle input outside integers between 1 and 9 range

--- lib/player.rb
+++ lib/player.rb
@@ -7,8 +7,14 @@ class Player
     @mark = mark
   end
 
-  def provide_move
+  def provide_move(display1)
     location = gets.chomp.to_i
+    if location.integer? && location.between?(1,9)
+      location
+    else
+      display1.display_message("Please enter an integer between 1 and 9 (inclusive): ")
+      provide_move(display1)
+    end
   end
 
 end
