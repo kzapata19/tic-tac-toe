37ad64ca79e1a8a7085a5e2dc5a6a654249d8a11
Karen Zapata <kzapata0919@gmail.com>
2017-12-01 02:15:31 -0800

    Remove comments and initiate game with run_game_loop

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,28 +3,12 @@ require './lib/player'
 require './lib/board'
 require './lib/rules'
 require './lib/display'
+# require './lib/validation'
 
 player1 = Player.new("X")
 player2 = Player.new("O")
 board = Board.new(3)
 rules = Rules.new(board)
-display1 = Display.new
-current_player = player1
+display1 = Display.new(board)
 game_session = GameSession.new(player1, player2, board, rules, display1)
-game_session.start_game
-
-=begin
-  to play game in terminal
-    - add "print" to display_board method in display.rb
-    - inside game_session.rb uncomment:
-      1.   start_game method: provide_move(@current_player)
-      2.   provide_move method: location = gets.chomp.to_i
-
-  This will
-    1. print the stringified board after the welcome message
-    2. will prompt for Player 1's move
-    3. will then take move
-    4. translate location for proper placement in nested array
-    5. place in nested array
-    6. print out updated stringied board to terminal
-=end
+game_session.run_game_loop
