de5c269055dc0db6ca6191acb21526b14b7d8517
Karen Zapata <kzapata0919@gmail.com>
2017-12-06 13:40:00 -0800

    Add

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,30 @@
 class Board
 
+  class InvalidBoardSizeError < StandardError
+    # attr_reader :action
+
+    # def initialize(message)
+    #   # call parent's constructor to set the message
+    #   super(message)
+    #   # @action = action
+    # end
+
+  end
+
   attr_accessor :size, :grid
 
   def initialize(size=3)
     @size = size
-    @grid = Array.new(size){Array.new(size)}
+    @grid = create_grid
+  end
+
+  def create_grid
+    raise InvalidBoardSizeError.new("Invalid board size") if @size < 2
+    Array.new(@size){Array.new(@size)}
   end
 
 end
+
+# board1 = Board.new(1)
+# p board1.grid
+# p board1.create_grid
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,25 +2,33 @@ describe Board do
 
   context "at the start of the game" do
 
-    it "grid should be empty" do
+    it "should be an empty grid" do
 
-      @board = Board.new(3)
-      expect(@board.grid).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
+      board = Board.new(3)
+      expect(board.create_grid).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
     end
 
     it "should create a 3x3 grid by default" do
-      @board = Board.new
-      expect(@board.grid).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
+      board = Board.new
+      expect(board.create_grid).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
     end
 
   end
 
-  #REACH
-  # it "should create a grid larger than 1x1" do
-  #   @board = Board.new(1)
-  #   expect(@board.valid_size?).to eq("Enter a board size larger than 1")
-  # end
+  it "should be able to customize it's grid size" do
+    board = Board.new(4)
+    expect(board.create_grid).to eq([[nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil], [nil, nil, nil, nil]])
+  end
+
+  it "should raise error if grid size is less than 2x2" do
+    board = Board.new(1)
+    # @board.create_grid
+    expect(board).to raise_error(Board::InvalidBoardSizeError)
+  end
+
+  it "should have a grid size" do
+    board = Board.new(4)
+    expect(board.size).to eq(4)
+  end
 
 end
-
-
