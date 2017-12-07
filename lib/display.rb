09b4c154d7785472f12fdf3ea6169107fbe807f1
Karen Zapata <kzapata0919@gmail.com>
2017-12-07 15:45:10 -0800

    Moved all print messages from GameSession into Display, added tests

--- lib/display.rb
+++ lib/display.rb
@@ -1,13 +1,43 @@
 class Display
 
-  attr_reader :output
+  attr_reader :input, :output
 
-  def initialize(output=STDOUT)
+  def initialize(input=STDIN, output=STDOUT)
+    @input = input
     @output = output
+  end
+
+  def print_welcome
+    display_message("Welcome to TicTacToe! Player 1 is 'X' and Player 2 is 'O'.")
+  end
+
+  def get_board_size
+    display_message("Please provide a number for the board size to build a proportional board: ")
+    save_player_input
+  end
+
+  def print_current_player_turn(player)
+    display_message("Player #{player.mark}'s turn: ")
+  end
+
+  def print_interim_msg
+    display_message("No winner or tie yet...")
+  end
+
+  def print_winner(player)
+    display_message("#{player.mark} is the winner!\n")
+  end
+
+  def print_tie
+    display_message("No winner. Tie game!\n")
   end
 
   def display_message(msg)
     @output.print msg
+  end
+
+  def save_player_input
+    @input.gets
   end
 
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -2,21 +2,55 @@ describe Display do
 
   before(:each) do
     @board = Board.new
+    @input = StringIO.new
     @output = StringIO.new
-    @display1 = Display.new(@output)
+    @display1 = Display.new(@input, @output)
   end
 
   context "at the start of the game" do
+
+    it "should display a welcome message" do
+    @display1.print_welcome
+    expect(@output.string).to eq("Welcome to TicTacToe! Player 1 is 'X' and Player 2 is 'O'.")
+    end
+
+    it "should ask for board size" do
+      @display1.get_board_size
+      expect(@output.string).to eq("Please provide a number for the board size to build a proportional board: ")
+    end
+
     it "should display an empty board" do
       @display1.display_message(@board.game_board)
       expect(@output.string).to eq(
       "\n_1_|_2_|_3_|\n\n_4_|_5_|_6_|\n\n_7_|_8_|_9_|\n\n")
     end
+
   end
 
-  it "should display given message" do
-    @display1.display_message("Welcome to TicTacToe!")
-    expect(@output.string).to eq("Welcome to TicTacToe!")
+  context "when no win or tie" do
+
+    it "should display an interim message" do
+      @display1.print_interim_msg
+      expect(@output.string).to eq("No winner or tie yet...")
+    end
+
+  end
+
+  it "should display current player's turn" do
+    @current_player = Player.new("X")
+    @display1.print_current_player_turn(@current_player)
+    expect(@output.string).to eq("Player X's turn: ")
+  end
+
+  it "should display the winner's mark" do
+    @winner = Player.new("X")
+    @display1.print_winner(@winner)
+    expect(@output.string).to eq("X is the winner!\n")
+  end
+
+  it "should display a tie message" do
+    @display1.print_tie
+    expect(@output.string).to eq("No winner. Tie game!\n")
   end
 
 end
