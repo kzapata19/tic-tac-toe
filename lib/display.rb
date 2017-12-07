d4c67ddbb5117fa07071e62986d1d3de04d29093
Karen Zapata <kzapata0919@gmail.com>
2017-12-06 16:22:02 -0800

    Modify spec and code for displaying game_board, remove display_board method

--- lib/display.rb
+++ lib/display.rb
@@ -1,16 +1,9 @@
 class Display
 
-  attr_reader :board, :output, :board_numbers
+  attr_reader :output
 
-  def initialize(board, output=STDOUT)
-    @board = board
+  def initialize(output=STDOUT)
     @output = output
-    @board_numbers = (1..board.size**2).to_a
-  end
-
-  def display_board
-    display_message(
-     "\n_#{@board_numbers[0]}_|_#{@board_numbers[1]}_|_#{@board_numbers[2]}_\n\n_#{@board_numbers[3]}_|_#{@board_numbers[4]}_|_#{@board_numbers[5]}_\n\n_#{@board_numbers[6]}_|_#{@board_numbers[7]}_|_#{@board_numbers[8]}_\n\n")
   end
 
   def display_message(msg)
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -3,14 +3,14 @@ describe Display do
   before(:each) do
     @board = Board.new
     @output = StringIO.new
-    @display1 = Display.new(@board, @output)
+    @display1 = Display.new(@output)
   end
 
   context "at the start of the game" do
     it "should display an empty board" do
-      @display1.display_board
+      @display1.display_message(@board.game_board)
       expect(@output.string).to eq(
-      "\n_1_|_2_|_3_\n\n_4_|_5_|_6_\n\n_7_|_8_|_9_\n\n")
+      "\n_1_|_2_|_3_|\n\n_4_|_5_|_6_|\n\n_7_|_8_|_9_|\n\n")
     end
   end
 
