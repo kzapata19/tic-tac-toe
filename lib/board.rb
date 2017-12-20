b8d7cb4bfd924d5496804c38cea0595336e7ea68
Karen Zapata <kzapata0919@gmail.com>
2017-12-20 11:55:20 -0800

    Add logic to prepend zero to space num label

--- lib/board.rb
+++ lib/board.rb
@@ -17,7 +17,7 @@ class Board
   def initialize(size=3)
     @size = size
     # self.check_board_size
-    @grid = create_grid
+    @grid = Array.new(@size){Array.new(@size)}
     @board_numbers = (1..@size**2).to_a
     @game_board = create_board
   end
@@ -32,21 +32,34 @@ class Board
     end
   end
 
-  def create_grid
-    Array.new(@size){Array.new(@size)}
-  end
+  # def create_grid
+  #   Array.new(@size){Array.new(@size)}
+  # end
+
 
   def create_board
     board = "\n"
-    length = @board_numbers.length
-    length.times do |number|
-      if (number + 1) % @size == 0
-        board = board + "_#{number + 1}_|" + "\n\n"
+    board_numbers = format_numbers
+    # length = board_numbers.length
+
+    board_numbers.each do |number|
+      if (number.to_i) % @size == 0
+        board = board + "_#{number}_|" + "\n\n"
       else
-        board = board + "_#{number + 1}_|"
+        board = board + "_#{number}_|"
       end
     end
     board
+  end
+
+  def prepend_leading_zero(number)
+    "%02d" % number if number.digits.count == 1
+  end
+
+  def format_numbers
+    @board_numbers.to_a.reduce([]) do|formatted, number|
+      formatted.push(prepend_leading_zero(number) || number)
+    end
   end
 
 end
