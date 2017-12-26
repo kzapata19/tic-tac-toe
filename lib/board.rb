db89a6d69a371e9c7d398d8be89efebe0be9157e
Karen Zapata <kzapata0919@gmail.com>
2017-12-26 14:12:12 -0800

    Remove space labeling and string board display from Board class. Initialize grid to have numbers instead of nil values. Add update_grid method and spec. All board specs pass

--- lib/board.rb
+++ lib/board.rb
@@ -17,29 +17,35 @@ class Board
   def initialize(size=3)
     @size = size
     self.check_board_size
-    @grid = Array.new(@size**2)
-    @board_numbers = (1..@size**2).to_a
-    @game_board = create_board
+    @grid = (1..@size**2).to_a
+    # @board_numbers = (1..@size**2).to_a
+    # @game_board = create_board
   end
 
   def check_board_size
     raise VoidBoardSizeError.new("Board size must be a positive integer greater than 2.") if @size < 3
   end
 
-  def create_board
-    board = "\n"
-    board_numbers = format_numbers
-    #use @board numbers, check if number then prepend 0 otherwise, do nothing to player's mark
-
-    board_numbers.each do |number|
-      if (number.to_i) % @size == 0
-        board = board + "_#{number}_|" + "\n\n"
-      else
-        board = board + "_#{number}_|"
-      end
-    end
-    board
+  def update_grid(location, mark)
+    converted_location = location - 1
+    @grid[converted_location] = mark
+    @grid
   end
+
+  # def create_board
+  #   board = "\n"
+  #   board_numbers = format_numbers
+  #   #use @board numbers, check if number then prepend 0 otherwise, do nothing to player's mark
+
+  #   board_numbers.each do |number|
+  #     if (number.to_i) % @size == 0
+  #       board = board + "_#{number}_|" + "\n\n"
+  #     else
+  #       board = board + "_#{number}_|"
+  #     end
+  #   end
+  #   board
+  # end
 
   # def label_board_spaces
   #   board = "\n"
@@ -56,14 +62,14 @@ class Board
   #   @grid.each
   # end
 
-  def format_numbers
-    @board_numbers.reduce([]) do|formatted, number|
-      formatted.push(prepend_leading_zero(number) || number)
-    end
-  end
+  # def format_numbers
+  #   @board_numbers.reduce([]) do|formatted, number|
+  #     formatted.push(prepend_leading_zero(number) || number)
+  #   end
+  # end
 
-  def prepend_leading_zero(number)
-    "%02d" % number if number.digits.count == 1
-  end
+  # def prepend_leading_zero(number)
+  #   "%02d" % number if number.digits.count == 1
+  # end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,19 +4,19 @@ describe Board do
 
     it "should have an empty grid" do
       @board = Board.new(3)
-      expect(@board.grid).to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil])
+      expect(@board.grid).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
 
     it "should create a 3x3 grid by default" do
       @board = Board.new
-      expect(@board.grid).to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil])
+      expect(@board.grid).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
 
   end
 
   it "should have a customizable size" do
     @board = Board.new(4)
-    expect(@board.grid).to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil])
+    expect(@board.grid).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
   end
 
   it "should raise error if board size is less than 3" do
@@ -28,9 +28,15 @@ describe Board do
     expect(@board.size).to eq(4)
   end
 
-  it "should create the game board" do
-    @board = Board.new(4)
-    expect(@board.game_board).to eq("\n_01_|_02_|_03_|_04_|\n\n_05_|_06_|_07_|_08_|\n\n_09_|_10_|_11_|_12_|\n\n_13_|_14_|_15_|_16_|\n\n")
+  it "should update the grid with player's mark" do
+    @board = Board.new(3)
+    expect(@board.update_grid(1, "X")).to eq(["X", 2, 3, 4, 5, 6, 7, 8, 9])
   end
+
+# this might have to go to Display not Board
+  # it "should create the game board" do
+  #   @board = Board.new(4)
+  #   expect(@board.game_board).to eq("\n_01_|_02_|_03_|_04_|\n\n_05_|_06_|_07_|_08_|\n\n_09_|_10_|_11_|_12_|\n\n_13_|_14_|_15_|_16_|\n\n")
+  # end
 
 end
