9269c482dc74978d1b159a22574f41cbab243a03
Karen Zapata <kzapata0919@gmail.com>
2017-12-26 14:41:08 -0800

    Refactor Player to only provide move and board size. Error handling for both inputs moved from Player to Board. All Player specs passing

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,5 @@
 class Player
 
-  class VoidMoveError < StandardError
-    def initialize(message)
-      super(message)
-    end
-  end
-
   class VoidBoardSizeError < StandardError
     def initialize(message)
       super(message)
@@ -19,22 +13,17 @@ class Player
     @input = input
   end
 
-  def provide_move(size)
+  def provide_move
     board_location = @input.gets.to_i
-    if board_location.integer? && board_location.between?(1, size)
-      board_location
-    else
-      raise VoidMoveError.new("Provide a number between 1 and #{size} inclusive")
-    end
   end
 
   def provide_board_size
     board_size = @input.gets.to_i
-    if board_size.integer? && board_size > 2
-      board_size
-    else
-      raise VoidBoardSizeError.new("Board size must be a positive integer greater than 2.")
-    end
+    # if board_size.integer? && board_size > 2
+    #   board_size
+    # else
+    #   raise VoidBoardSizeError.new("Board size must be a positive integer greater than 2.")
+    # end
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -18,17 +18,9 @@ describe Player do
     expect(@player1.mark).to eq("X")
   end
 
-# this will break because no size arg passed
   it "should provide a board move" do
     @input.string = "1"
-    @board = Board.new
-    expect(@player1.provide_move(@board.size)).to eq(1)
-  end
-
-  it "should raise an error if move is outside game board size" do
-    @input.string = "17"
-    @board = Board.new(4)
-    expect {@player1.provide_move(@board.size)}.to raise_error(Player::VoidMoveError)
+    expect(@player1.provide_move).to eq(1)
   end
 
 end
