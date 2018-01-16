9e9c8bb39a6b0becc8f8001c082ff788d0a4e1f1
Karen Zapata <kzapata0919@gmail.com>
2018-01-16 12:42:29 -0800

    Edit error message

--- lib/initializer.rb
+++ lib/initializer.rb
@@ -20,12 +20,12 @@ class Initializer
   def get_board_size
     size = 0
     @output.print "Please provide a number for the board size to build a proportional board: "
-    while size < 3
+    until size >= 3
       begin
         size = @input.gets.to_i
-        raise VoidBoardSizeError.new("Board size must be a positive integer greater than 2. Try again...")
+        raise VoidBoardSizeError.new("Board size '#{size}' invalid. Size must be a positive integer greater than 2. Try again...")
       rescue VoidBoardSizeError=>e
-        @output.print e.message
+        @output.print "#{e.message}\n"
       end
     end
     size
