ed7f368302afe2771edd4a9cd6c0737d18ab16a9
Karen Zapata <kzapata0919@gmail.com>
2018-01-18 20:33:34 -0800

    Rename display1 to display

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -4,23 +4,23 @@ describe Display do
     @board = Board.new(3)
     @input = StringIO.new
     @output = StringIO.new
-    @display1 = Display.new(@input, @output)
+    @display = Display.new(@input, @output)
   end
 
   context "at the start of the game" do
 
     it "should display a welcome message" do
-    @display1.print_welcome
+    @display.print_welcome
     expect(@output.string).to eq("Welcome to TicTacToe! Player 1 is 'X' and Player 2 is 'O'.\n")
     end
 
     it "should ask for board size" do
-      @display1.get_board_size
+      @display.get_board_size
       expect(@output.string).to eq("Please provide a number for the board size to build a proportional board: ")
     end
 
     it "should display an empty board of spaces labeled with prepended zeroes" do
-      @display1.print_game_board(@board.grid)
+      @display.print_game_board(@board.grid)
       expect(@output.string).to eq(
       "\n_01_|_02_|_03_|\n\n_04_|_05_|_06_|\n\n_07_|_08_|_09_|\n\n")
     end
@@ -30,7 +30,7 @@ describe Display do
   context "when no win or tie" do
 
     it "should display an interim message" do
-      @display1.print_interim_msg
+      @display.print_interim_msg
       expect(@output.string).to eq("No winner or tie yet...")
     end
 
@@ -38,18 +38,18 @@ describe Display do
 
   it "should display current player's turn" do
     @current_player = Player.new(" X")
-    @display1.print_current_player_turn(@current_player)
+    @display.print_current_player_turn(@current_player)
     expect(@output.string).to eq("Player X's turn: ")
   end
 
   it "should display the winner's mark" do
     @winner = Player.new("X")
-    @display1.print_winner(@winner)
+    @display.print_winner(@winner)
     expect(@output.string).to eq("X is the winner!\n")
   end
 
   it "should display a tie message" do
-    @display1.print_tie
+    @display.print_tie
     expect(@output.string).to eq("No winner. Tie game!\n")
   end
 
