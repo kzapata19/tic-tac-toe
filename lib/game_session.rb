3e83fc2df67e85ed9f5b53fcc063801bc1788e85
Karen Zapata <kzapata0919@gmail.com>
2018-01-18 20:49:32 -0800

    Replace display1 with display, update else statement for display_current_board_status and simplify switch_turns

--- lib/game_session.rb
+++ lib/game_session.rb
@@ -6,12 +6,12 @@ class GameSession
     end
   end
 
-  attr_accessor :player1, :player2, :display1, :outcome, :board_size, :board, :current_player
+  attr_accessor :player1, :player2, :display, :outcome, :board_size, :board, :current_player
 
-  def initialize(player1, player2, display1, outcome, initializer)
+  def initialize(player1, player2, display, outcome, initializer)
     @player1 = player1
     @player2 = player2
-    @display1 = display1
+    @display = display
     @outcome = outcome
     @board = initializer.board
     @current_player = @player2
@@ -19,8 +19,8 @@ class GameSession
   end
 
   def start_game
-    @display1.print_welcome
-    @display1.print_game_board(@board.grid)
+    @display.print_welcome
+    @display.print_game_board(@board.grid)
     run_game_loop
   end
 
@@ -33,7 +33,7 @@ private
         raise VoidMoveError.new("'#{location}' is out of range. Provide a number between 1 and #{@board.grid.size} inclusive. Try again...")
       end
     rescue VoidMoveError=>e
-       @display1.display_message("#{e.message}\n")
+       @display.display_message("#{e.message}\n")
       retry
     end
     update_board(location, @current_player.mark)
@@ -47,28 +47,28 @@ private
   def display_current_board_status
     full_board = @board.full_board?(@board.grid)
 
-    if @outcome.winner?(@board.get_all_move_combos)
-      @display1.print_winner(@current_player)
+    if @outcome.has_winner?(@board.get_all_move_combos)
+      @display.print_winner(@current_player)
     elsif @outcome.is_tie?(@board.get_all_move_combos, full_board)
-      @display1.print_tie
-    else !@outcome.winner?(@board.get_all_move_combos) || !@outcome.is_tie?(@board.get_all_move_combos)
-      @display1.print_interim_msg
+      @display.print_tie
+    else
+      @display.print_interim_msg
       run_game_loop
     end
   end
 
   def switch_turns
-    @current_player != player1 ? @current_player = player1 : @current_player = player2
+    @current_player = (@current_player == player2) ? player1 : player2
   end
 
   def get_player_move
-    @display1.print_current_player_turn(@current_player)
+    @display.print_current_player_turn(@current_player)
     @current_player.provide_move
   end
 
   def update_board(location, player_mark)
     @board.update_grid(location, player_mark)
-    @display1.print_game_board(@board.grid)
+    @display.print_game_board(@board.grid)
   end
 
 end
