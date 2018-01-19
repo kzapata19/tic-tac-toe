be869b74070cfd2bc3ff93657dec545de037794c
Karen Zapata <kzapata0919@gmail.com>
2018-01-18 20:36:14 -0800

    Rename winner? method and is_tie? param

--- lib/outcome.rb
+++ lib/outcome.rb
@@ -1,10 +1,10 @@
 class Outcome
 
-  def is_tie?(all_move_combos, full_board_bool)
-    full_board_bool && !winner?(all_move_combos) ? true : false
+  def is_tie?(all_move_combos, is_board_full)
+    is_board_full && !has_winner?(all_move_combos)
   end
 
-  def winner?(all_move_combos)
+  def has_winner?(all_move_combos)
     winner = false
     all_move_combos.each do |set_of_moves|
       if set_of_moves.uniq.length == 1
