63cf95ac81c302741113404b400131ee60db58c0
Karen Zapata <kzapata0919@gmail.com>
2018-01-18 20:29:03 -0800

    Rename display1 to display

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,9 +7,9 @@ require './lib/initializer'
 
 player1 = Player.new(' X')
 player2 = Player.new(' O')
-display1 = Display.new
+display = Display.new
 outcome = Outcome.new
 initializer = Initializer.new
 
-game_session = GameSession.new(player1, player2, display1, outcome, initializer)
+game_session = GameSession.new(player1, player2, display, outcome, initializer)
 game_session.start_game
