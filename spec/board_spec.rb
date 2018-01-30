79ad8d832375994c4d13db212352aee7d4571ee8
Karen Zapata <kzapata0919@gmail.com>
2018-01-30 13:56:54 -0800

    Replace before hook and instance variables with let

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,31 +1,29 @@
 describe Board do
-  before(:each) do
-    @board = Board.new(3)
-  end
+  let(:board) {Board.new(3)}
 
-  context "at the start of the game" do
+  context "when at the start of the game" do
 
     it "returns an array with numbered spaces" do
-      expect(@board.grid).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      expect(board.grid).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
 
   end
 
   it "has a customizable size" do
-    @board = Board.new(4)
-    expect(@board.grid).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+    board = Board.new(4)
+    expect(board.grid).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
   end
 
   it "has a board size" do
-    expect(@board.size).to eq(3)
+    expect(board.size).to eq(3)
   end
 
   it "updates the grid with player's mark" do
-    expect(@board.update_grid(1, "X")).to eq(["X", 2, 3, 4, 5, 6, 7, 8, 9])
+    expect(board.update_grid(1, "X")).to eq(["X", 2, 3, 4, 5, 6, 7, 8, 9])
   end
 
   it "returns each row's content in an array" do
-    expect(@board.get_all_rows).to eq([
+    expect(board.get_all_rows).to eq([
       [1,2,3],
       [4,5,6],
       [7,8,9]
@@ -33,7 +31,7 @@ describe Board do
   end
 
   it "returns each column's content in an array" do
-    expect(@board.get_all_columns).to eq([
+    expect(board.get_all_columns).to eq([
       [1,4,7],
       [2,5,8],
       [3,6,9]
@@ -41,19 +39,19 @@ describe Board do
   end
 
   it "returns the antidiagonal's content in an array" do
-    expect(@board.get_antidiagonal).to eq([3,5,7])
+    expect(board.get_antidiagonal).to eq([3,5,7])
   end
 
   it "returns the main diagonal's content in an array" do
-    expect(@board.get_main_diagonal).to eq([1,5,9])
+    expect(board.get_main_diagonal).to eq([1,5,9])
   end
 
   it "retuns both diagonals in an array" do
-    expect(@board.get_diagonals).to eq([[1,5,9], [3,5,7]])
+    expect(board.get_diagonals).to eq([[1,5,9], [3,5,7]])
   end
 
   it "returns all possible move combinations for a 3x3 board" do
-    expect(@board.get_all_move_combos).to eq([
+    expect(board.get_all_move_combos).to eq([
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9],
@@ -66,8 +64,8 @@ describe Board do
   end
 
   it "returns all possible move combinations for a 4x4 board" do
-    @board = Board.new(4)
-    expect(@board.get_all_move_combos).to eq([
+    board = Board.new(4)
+    expect(board.get_all_move_combos).to eq([
       [1, 2, 3, 4],
       [5, 6, 7, 8],
       [9, 10, 11, 12],
@@ -82,12 +80,12 @@ describe Board do
   end
 
   it "returns false when board is empty" do
-    expect(@board.full?(@board.grid)).to be_falsey
+    expect(board.full?(board.grid)).to be_falsey
   end
 
   it "returns true if board is full" do
-    @board.grid = ['X', 'X', 'O', 'X', 'O', 'O', 'X', 'O', 'X']
-    expect(@board.full?(@board.grid)).to be_truthy
+    board.grid = ['X', 'X', 'O', 'X', 'O', 'O', 'X', 'O', 'X']
+    expect(board.full?(board.grid)).to be_truthy
   end
 
 end
